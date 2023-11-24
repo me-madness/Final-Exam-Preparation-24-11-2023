9aac7f115c9c351089d3f4a3b04d327bd9596f90
me_madness <p_rangel@abv.bg>
2023-11-24 19:48:28 +0100

    01.1 are also done from the lecture

--- 01. world_tour.py
+++ 01. world_tour.py
@@ -3,6 +3,7 @@
 
 
 # Second task from the lecture
+# 01
 
 stops = input()
 command = input().split(":")
@@ -25,4 +26,41 @@ while command[0] != "Travel":
     command = input().split(":")     
 
 
+print(f"Ready for world tour! Planned stops: {stops}")
+
+# Second option for this task from the lecture
+# 01.1
+ 
+def add_stop(stops, index, some_string):
+    if index in range(len(stops)):
+        stops = stops[:index] + some_string + stops[index:]
+    return stops
+ 
+ 
+def remove_stop(stops, start_index, end_index):
+    if start_index in range(len(stops)) and end_index in range(len(stops)):
+        stops = stops[:start_index] + stops[end_index + 1:]
+    return stops
+ 
+ 
+def switch(stops, old_string, new_string):
+    if old_string in stops:
+        stops = stops.replace(old_string, new_string)
+    return stops
+ 
+ 
+stops = input()
+command = input().split(":")
+while command[0] != "Travel":
+    if command[0] == "Add Stop":
+        index, some_string = int(command[1]), command[2]
+        stops = add_stop(stops, index, some_string)
+    elif command[0] == "Remove Stop":
+        start_index, end_index = int(command[1]), int(command[2])
+        stops = remove_stop(stops, start_index, end_index)
+    elif command[0] == "Switch":
+        old_string, new_string = command[1], command[2]
+        stops = switch(stops, old_string, new_string)
+    print(stops)
+    command = input().split(":")
 print(f"Ready for world tour! Planned stops: {stops}")
