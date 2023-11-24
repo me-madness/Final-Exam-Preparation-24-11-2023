4b189183d2b4c907f33712139913c4ca22e30bac
me_madness <p_rangel@abv.bg>
2023-11-24 18:21:56 +0100

    first print are done

--- 02. ad_astra.py
+++ 02. ad_astra.py
@@ -10,4 +10,5 @@ information = input()
 pattern = r"(?i)([#|])([a-z\s]+)(\1)(\d{2}\/\d{2}\/\d{2})(\1)(\d+)(\1)"
 matches = re.findall(pattern, information)
 total_calories = [sum([int(match[5]) for match in matches])]
-
+days = total_calories // 2000
+print(f"You have food to last you for: {days} days")
