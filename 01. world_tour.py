63c3f763c2b155817f63910dce91a28659b07f33
me_madness <p_rangel@abv.bg>
2023-11-24 17:40:53 +0100

    01 begin from the lecture

--- 01. world_tour.py
+++ 01. world_tour.py
@@ -4,3 +4,20 @@
 
 # Second task from the lecture
 
+stops = input()
+command = input().split(":")
+
+while command[0] != "Travel":
+    if command[0] == "Add Stop":
+        index, some_string = int(command[2], command[2])
+        if index in range(len(stops)):
+            stops = stops[:index] + some_string + stops[index:]
+    elif command[0] == "Remove Stop":
+        start_index, end_index = int(command[2], command[2])
+    elif command[0] == "Switch":
+         old_string, new_string= int(command[2], command[2])
+         
+    command = input().split(":")     
+
+
+print(f"Ready for world tour! Planned stops: {string}")
